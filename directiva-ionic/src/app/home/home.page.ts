b178c227d24db2817d2321c85403784016b0abaf
JosueGomez9 <123044553+JosueGomez9@users.noreply.github.com>
2024-02-10 17:30:08 -0600

    Router-Link

--- directiva-ionic/src/app/home/home.page.ts
+++ directiva-ionic/src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent,IonButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent,IonList,IonButton} from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonButton],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonList,IonButton,RouterLink],
 })
 export class HomePage {
   constructor() {}
